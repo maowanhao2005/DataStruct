40ff50b6479fbf5d9916e0312f54960f37a4b4a5
MaoWH <3554865186@qq.com>
2024-10-25 01:13:05 +0800

    栈点解释

--- SRC/stack.h
+++ SRC/stack.h
@@ -17,97 +17,97 @@ private:
 
     
 private:
-    Node<DataType>* top = NULL;
-    int size = 0;
+    Node<DataType>* top = NULL;                                     //空间为空
+    int size = 0;                                                   //空间为空
 public:
-    stack() = default;
+    stack() = default;                                             
     ~stack(){
-        while(top!=NULL){
-            Node<DataType>* temp = top;
-            top = top->next;
-            std::cout << "Deleting " << temp->data << std::endl;
-            delete temp;
+        while(top!=NULL){                                           //空间不为空的时候
+            Node<DataType>* temp = top;                             //让空间指针temp指向top
+            top = top->next;                                        //让top指向下一个
+            std::cout << "Deleting " << temp->data << std::endl;    //输出一句话和空间指针指向打他的值
+            delete temp;                                            //删除上面的空间
         }
-        std::cout << "Stack deleted" << std::endl;
+        std::cout << "Stack deleted" << std::endl;                  //输出一句话
     }
 
     /// @brief 将数据放入栈中存储
     /// @param data 将放入栈中的数据
     void push(DataType data){
-        if (top==NULL){
-            top = new Node<DataType>;
-            top->data = data;
-            top->next = NULL;
-            size++;
+        if (top==NULL){                                             //如果栈为空
+            top = new Node<DataType>;                               //空节点等于一个新建的空间
+            top->data = data;                                       //给新建的这个空间的data赋一个新data值
+            top->next = NULL;                                       //空间的下一个节点指向空
+            size++;                                                 //空间扩展
         }
-        else{
-            Node<DataType>* temp = new Node<DataType>;
-            temp->data = data;
-            temp->next = top;
-            top = temp;
-            size++;
+        else{                                                       //或者
+            Node<DataType>* temp = new Node<DataType>;              //空间指向一个变量等于一个新的空间
+            temp->data = data;                                      //给这个新的空间赋一个新的data的值
+            temp->next = top;                                       //新的空间的下一个指针指向下面的节点
+            top = temp;                                             //让top指针指向新空间
+            size++;                                                 //空间扩展
         }
-        std::cout << "Pushed: " << data << ",size = "<< size << std::endl;
+        std::cout << "Pushed: " << data << ",size = "<< size << std::endl;//输出一句话加上data的值再输出一句话加上shize的值
     }
 
     DataType pop(){
-        if (top==NULL){
-            std::cout << "Stack is empty" << std::endl;
-            return 0;
+        if (top==NULL){                                             //如果空间为空
+            std::cout << "Stack is empty" << std::endl;             //输出一句话
+            return 0;                                               //返回值
         }
-        else{
-            DataType data = top->data;
-            Node<DataType>* temp = top;
-            top = top->next;
-            delete temp;
-            size--;
-            std::cout << "Popped: " << data << ",size = "<< size << std::endl;
-            return data;
+        else{                                                       //或者
+            DataType data = top->data;                              //让data的top指向新的data
+            Node<DataType>* temp = top;                             //让空间指向top
+            top = top->next;                                        //让top指向下一个
+            delete temp;                                            //删除最上面的空间
+            size--;                                                 //空间删除
+            std::cout << "Popped: " << data << ",size = "<< size << std::endl;//输出一句话
+            return data;                                            //返回data的值
         }
     }
 
     DataType peek(){
-        if (top==NULL){
-            std::cout << "Stack is empty" << std::endl;
-            return 0;
+        if (top==NULL){                                             //如果空间为空
+            std::cout << "Stack is empty" << std::endl;             //输出一句话
+            return 0;                                               //返回值
         }
-        else{
-            return top->data;
+        else{                                                       //或者
+            return top->data;                                       //返回最上面的top指向的data
         }
     }
 
     bool isEmpty(){
-        return top==NULL;
+        return top==NULL;                                          //判断空间是不是空
     }
 
     int getSize(){
-        return size;
+        return size;                                                //返回size的值
     }
 
-    void print(){
-        Node<DataType>* temp = top;
+    void print(){                                                   
+        Node<DataType>* temp = top;                                 //
         while(temp!=NULL){
-            std::cout << temp->data << " ";
-            temp = temp->next;
+            std::cout << temp->data << " ";                          //输出temp指向的data的值
+            temp = temp->next;                                      //让temp指向下一个
         }
-        std::cout << std::endl;
+        std::cout << std::endl;                                     //换行
     }
 
     void clear(){
-        while(top!=NULL){
-            Node<DataType>* temp = top;
-            top = top->next;
-            delete temp;
+        while(top!=NULL){                                           //空间不等于空的时候
+            Node<DataType>* temp = top;                             //让空间的temp指向top
+            top = top->next;                                        //让top指向下一个
+            delete temp;                                            //删除temp
         }
-        size = 0;
-        std::cout << "Stack cleared" << std::endl;
+        size = 0;                                                   //空间等于0
+        std::cout << "Stack cleared" << std::endl;                  //输出一句话
     }
 
 };
 
 void debugStack(){
-    using namespace std;
-    stack<char> s;
+    using namespace std;                                            //使用std输出
+    stack<char> s;                                                  //定义stack为s
 
     s.push('a');
     s.push('b');
@@ -115,15 +115,15 @@ void debugStack(){
     s.push('d');
     s.push('e');
     
-    cout << "size = " << s.getSize() << endl;   
+    cout << "size = " << s.getSize() << endl;                      //输出size
 
-    s.print();
+    s.print();                                                     //输出
 
-    while(!s.isEmpty()){
-        cout << s.pop() << endl;
+    while(!s.isEmpty()){                                           ///当size不为空的时候
+        cout << s.pop() << endl;                                   //输出
     }
 
-    cout << "size = " << s.getSize() << endl;
+    cout << "size = " << s.getSize() << endl;                       //输出size
 
     s.push('f');
 }
